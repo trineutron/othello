c3c0f845fe25daa5bc7714de273c13551755b225
trineutron <programgoo@gmail.com>
2021-03-23 02:53:39 +0900

    石を返す処理を追加

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,70 @@
-const board = document.createElement('table');
-document.getElementById('board').appendChild(board);
+const boardShow = document.createElement('table');
+document.getElementById('board').appendChild(boardShow);
 for (let i = 0; i < 8; i++) {
     const boardLine = document.createElement('tr');
-    board.appendChild(boardLine);
+    boardShow.appendChild(boardLine);
     for (let j = 0; j < 8; j++) {
         const boardCell = document.createElement('td');
         boardLine.appendChild(boardCell);
         boardCell.addEventListener('click', makeMove);
-        boardCell.setAttribute('id', 9 * i + j + 1);
+        boardCell.setAttribute('id', 9 * i + j + 10);
     }
 }
 
-let color = 'black';
-
-function nextTurn() {
-    if (color === 'black') {
-        color = 'white';
+let board = [];
+for (let i = 0; i < 91; i++) {
+    if (i < 10 || 80 < i || i % 9 === 0) {
+        board.push('wall');
     } else {
-        color = 'black';
+        board.push('empty');
     }
 }
+
+let color;
+
+function flip(idx) {
+    board[idx] = color;
+    document.getElementById(idx).setAttribute('class', color);
+}
+
+color = 'white'
+flip(40);
+flip(50);
+
+color = 'black'
+flip(41);
+flip(49);
+
+function opponent() {
+    if (color === 'black') {
+        return 'white';
+    } else {
+        return 'black';
+    }
+}
+
+const directions = [-10, -9, -8, -1, 1, 8, 9, 10];
 
 function makeMove() {
-    if (this.getAttribute('class')) return; // 石が重なる
-    this.setAttribute('class', color);
-    nextTurn();
+    const idx = Number(this.getAttribute('id'));
+    if (board[idx] !== 'empty') return;
+    let movable = false;
+    directions.forEach(d => {
+        let next = idx + d;
+        while (board[next] === opponent(color)) {
+            next += d;
+        }
+        if (board[next] === color) {
+            next -= d;
+            while (board[next] === opponent(color)) {
+                flip(next);
+                movable = true;
+                next -= d;
+            }
+        }
+    });
+    if (movable) {
+        flip(idx);
+        color = opponent(color);
+    }
 }
