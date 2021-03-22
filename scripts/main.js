f1a6893bcbbfd9d636a8b659d9a5cfa122290519
trineutron <programgoo@gmail.com>
2021-03-22 23:51:35 +0900

    HTMLの自動生成

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
-const myCells = document.querySelectorAll('td');
+const board = document.createElement('table');
+document.getElementById('board').appendChild(board);
+for (let i = 0; i < 8; i++) {
+    const boardLine = document.createElement('tr');
+    board.appendChild(boardLine);
+    for (let j = 0; j < 8; j++) {
+        const boardCell = document.createElement('td');
+        boardLine.appendChild(boardCell);
+        boardCell.addEventListener('click', makeMove);
+        boardCell.setAttribute('id', 9 * i + j + 1);
+    }
+}
 
 let color = 'black';
+
 function nextTurn() {
     if (color === 'black') {
         color = 'white';
@@ -9,12 +21,8 @@ function nextTurn() {
     }
 }
 
-for (let i = 0; i < myCells.length; i++) {
-    const myCell = myCells[i];
-    myCell.onclick = function () {
-        if (!myCell.getAttribute('class')) {
-            myCell.setAttribute('class', color);
-            nextTurn();
-        }
-    }
+function makeMove() {
+    if (this.getAttribute('class')) return; // 石が重なる
+    this.setAttribute('class', color);
+    nextTurn();
 }
