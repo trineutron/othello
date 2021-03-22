c1d5ac169bd0a7d478b44b56c08f5d687d3b64f6
trineutron <programgoo@gmail.com>
2021-03-22 22:32:03 +0900

    石を打つ処理を追加

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
-const myCell = document.querySelector('td');
-myCell.onclick = function () {
-    myCell.setAttribute('class', 'black');
+const myCells = document.querySelectorAll('td');
+
+let color = 'black';
+function nextTurn() {
+    if (color === 'black') {
+        color = 'white';
+    } else {
+        color = 'black';
+    }
+}
+
+for (let i = 0; i < myCells.length; i++) {
+    const myCell = myCells[i];
+    myCell.onclick = function () {
+        if (!myCell.getAttribute('class')) {
+            myCell.setAttribute('class', color);
+            nextTurn();
+        }
+    }
 }
