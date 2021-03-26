181bb1b70f8ffbe8a13d53722854f7a70d0c9512
trineutron <programgoo@gmail.com>
2021-03-27 01:21:26 +0900

    評価関数の余計な処理をなくして8手読みを実装

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+const black = 1, white = -1, empty = 0, wall = 2, end = 0;
+
+function colorString(color) {
+    if (color === 1) {
+        return 'black';
+    } else {
+        return 'white';
+    }
+}
+
 // オセロ盤の表示
 const boardShow = document.createElement('table');
 document.getElementById('board').appendChild(boardShow);
@@ -16,18 +26,18 @@ for (let i = 0; i < 8; i++) {
 let board = [];
 for (let i = 0; i < 91; i++) {
     if (i < 10 || 80 < i || i % 9 === 0) {
-        board.push('wall');
+        board.push(wall);
     } else if (i === 40 || i === 50) {
-        board.push('white');
+        board.push(white);
         document.getElementById(i).setAttribute('class', 'white');
     } else if (i === 41 || i === 49) {
-        board.push('black');
+        board.push(black);
         document.getElementById(i).setAttribute('class', 'black');
     } else {
-        board.push('empty');
+        board.push(empty);
     }
 }
-board.push('black');
+board.push(black);
 
 const directions = [-10, -9, -8, -1, 1, 8, 9, 10];
 
@@ -45,15 +55,15 @@ function changeColor(newBoard) {
 function flip(idx) {
     const color = getColor(board);
     board[idx] = color;
-    document.getElementById(idx).setAttribute('class', color);
+    document.getElementById(idx).setAttribute('class', colorString(color));
 }
 
 // 相手の色
 function opponent(color) {
-    if (color === 'black') {
-        return 'white';
+    if (color === black) {
+        return white;
     } else {
-        return 'black';
+        return black;
     }
 }
 
@@ -63,7 +73,7 @@ function listMovable(newBoard) {
     const color = getColor(newBoard);
     for (let i = 0; i < newBoard.length - 1; i++) {
         const cellState = newBoard[i];
-        if (cellState === 'empty') {
+        if (cellState === empty) {
             for (const d of directions) {
                 let next = i + d;
                 while (newBoard[next] === opponent(color)) {
@@ -87,7 +97,7 @@ function existsMovable(newBoard) {
     const color = getColor(newBoard);
     for (let i = 0; i < newBoard.length - 1; i++) {
         const cellState = newBoard[i];
-        if (cellState === 'empty') {
+        if (cellState === empty) {
             for (const d of directions) {
                 let next = i + d;
                 while (newBoard[next] === opponent(color)) {
@@ -108,14 +118,14 @@ function existsMovable(newBoard) {
 // クリック時の動作
 function makeMove() {
     const idx = Number(this.getAttribute('id'));
-    if (!human[getColor(board)] || board[idx] !== 'empty') return;
+    if (!human(getColor(board)) || board[idx] !== empty) return;
     move(idx);
 }
 
 // 着手
 function move(idx) {
     const color = getColor(board);
-    if (board[idx] !== 'empty') return;
+    if (board[idx] !== empty) return;
     let movable = false;
     for (const d of directions) {
         let next = idx + d;
@@ -134,27 +144,37 @@ function move(idx) {
     if (movable) {
         flip(idx);
         document.getElementById('countBlack').textContent = board.slice(0, 91).filter(function (x) {
-            return x === 'black';
+            return x === black;
         }).length;
         document.getElementById('countWhite').textContent = board.slice(0, 91).filter(function (x) {
-            return x === 'white';
+            return x === white;
         }).length;
         changeColor(board);
         const color = getColor(board);
         if (existsMovable(board)) {
-            document.getElementById('turn').textContent = color;
+            document.getElementById('turn').textContent = colorString(color);
         } else {
             changeColor(board);
             if (existsMovable(board)) {
-                alert(color + ' pass');
+                alert(colorString(color) + ' pass');
             } else {
-                board[91] = 'end';
+                board[91] = end;
                 document.getElementById('turn').textContent = '終局';
                 alert('終局');
             }
         }
-        if (getColor(board) !== 'end' && !human[getColor(board)]) {
-            move(moveByAI(defaultDepth));
+        if (getColor(board) !== end && !human(getColor(board))) {
+            let countEmpty = 0;
+            for (const state of board) {
+                if (state === empty) {
+                    countEmpty++;
+                }
+            }
+            if (countEmpty <= endgameDepth) {
+                move(moveByAI(countEmpty));
+            } else {
+                move(moveByAI(defaultDepth));
+            }
         }
     }
 }
@@ -181,7 +201,7 @@ function afterMove(oldBoard, idx) {
         if (!existsMovable(newBoard)) {
             changeColor(newBoard);
             if (!existsMovable(newBoard)) {
-                newBoard[91] = 'end';
+                newBoard[91] = end;
             }
         }
     }
@@ -190,14 +210,19 @@ function afterMove(oldBoard, idx) {
 
 // 黒番から見た評価値
 function evalBoard(newBoard) {
-    let res = 0, v = { 'black': 1, 'white': -1, 'empty': 0, 'wall': 0 };
-    if (getColor(newBoard) === 'end') {
+    let res = 0;
+    if (getColor(newBoard) === end) {
         for (let i = 0; i < newBoard.length - 1; i++) {
-            res += 1000 * v[newBoard[i]];
+            if (Math.abs(newBoard[i]) === 1) {
+                res += newBoard[i];
+            }
         }
         return res;
     }
     for (let i = 0; i < newBoard.length - 1; i++) {
+        if (Math.abs(newBoard[i]) !== 1) {
+            continue;
+        }
         let value = 0;
         if (i === 10 || i === 17 || i === 73 || i === 80) {  // 隅
             value = 16;
@@ -206,13 +231,13 @@ function evalBoard(newBoard) {
                 while (newBoard[next] === newBoard[i]) {
                     next += d;
                 }
-                if (newBoard[next] === 'empty') {
+                if (newBoard[next] === empty) {
                     value++;
                 }
             }
         }
         for (const d of directions) {
-            if (newBoard[i + d] === 'empty' && newBoard[i - d] !== 'wall') {
+            if (newBoard[i + d] === empty && newBoard[i - d] !== wall) {
                 value--;
                 if (i + d === 10 || i + d === 17 || i + d === 73 || i + d === 80) {
                     if (d === -10 || d === -8 || d === 8 || d === 10) {  // X
@@ -223,11 +248,13 @@ function evalBoard(newBoard) {
                             next -= d;
                         }
                         // 隅に繋がっておらず山でもない
-                        if (newBoard[next] !== 'wall' &&
-                            !(newBoard[next] === 'empty' && newBoard[next - d] === 'wall')) {
+                        if (newBoard[next] !== wall &&
+                            !(newBoard[next] === empty && newBoard[next - d] === wall)) {
                             value -= 8;
                             // ウイング
-                            if (newBoard[next] === 'empty' && newBoard[next - d] === 'empty' && newBoard[next - 2 * d] === 'wall') {
+                            if (newBoard[next] === empty
+                                && newBoard[next - d] === empty
+                                && newBoard[next - 2 * d] === wall) {
                                 value += 4;
                             }
                         }
@@ -235,19 +262,19 @@ function evalBoard(newBoard) {
                 }
             }
         }
-        res += value * v[newBoard[i]];
+        res += value * newBoard[i];
     }
     return res;
 }
 
 function moveByAI(depth) {
-    let movable = listMovable(board), res = [], maxScore = -65000;
+    let movable = listMovable(board), res = [], maxScore = -1000;
     const color = getColor(board);
     let newBoards = {}, evals = {};
     for (const idx of movable) {
         newBoards[idx] = afterMove(board, idx);
         evals[idx] = evalBoard(newBoards[idx]);
-        if (color === 'white') {
+        if (color === white) {
             evals[idx] *= -1;
         }
     }
@@ -256,7 +283,7 @@ function moveByAI(depth) {
     })
     for (const idx of movable) {
         const newBoard = newBoards[idx];
-        const eval = search(newBoard, depth - 1, color, maxScore - 1, 65000);
+        const eval = search(newBoard, depth - 1, color, maxScore - 1, 1000);
         if (eval > maxScore) {
             res = [idx];
             maxScore = eval;
@@ -269,42 +296,34 @@ function moveByAI(depth) {
 
 // 前の着手から見た評価値、α以下もしくはβ以上が確定したら枝刈り
 function search(currentBoard, depth, prevColor, alpha, beta) {
-    let empty = 0;
-    for (const state of currentBoard) {
-        if (state === 'empty') {
-            empty++;
-        }
-    }
-    if (depth === 0 && empty && empty <= additionalDepth) {
-        return search(currentBoard, empty, prevColor, alpha, beta);
-    }
     const color = getColor(currentBoard);
     let eval = alpha;
     if (prevColor !== color) {
         eval = -beta;
     }
     let movable = listMovable(currentBoard);
-    if (depth === 0 || color === 'end') {
+    if (depth === 0 || color === end) {
         eval = evalBoard(currentBoard);
-        if (prevColor === 'white') {
+        if (prevColor === white) {
             eval *= -1;
         }
         return eval;
     }
-    let newBoards = {}, evals = {};
-    for (const idx of movable) {
-        newBoards[idx] = afterMove(currentBoard, idx);
-        evals[idx] = evalBoard(newBoards[idx]);
-        if (color === 'white') {
-            evals[idx] *= -1;
+    if (depth > 1) {
+        let evals = {};
+        for (const idx of movable) {
+            evals[idx] = evalBoard(afterMove(currentBoard, idx));
+            if (color === white) {
+                evals[idx] *= -1;
+            }
         }
+        movable.sort(function (a, b) {
+            return evals[b] - evals[a];
+        });
     }
-    movable.sort(function (a, b) {
-        return evals[b] - evals[a];
-    })
     let first = true;
     for (const idx of movable) {
-        const newBoard = newBoards[idx];
+        const newBoard = afterMove(currentBoard, idx);
         let newAlpha = alpha, newBeta = alpha + 1;
         if (prevColor !== color) {
             newAlpha = -beta;
@@ -357,10 +376,17 @@ function search(currentBoard, depth, prevColor, alpha, beta) {
     return eval;
 }
 
-const defaultDepth = 6;
-const additionalDepth = 14 - defaultDepth;
-const human = { 'black': true, 'white': false };
+const defaultDepth = 8;
+const endgameDepth = 14;
 
-if (getColor(board) !== 'end' && !human[getColor(board)]) {
+function human(color) {
+    if (color === black) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (getColor(board) !== end && !human(getColor(board))) {
     move(moveByAI(defaultDepth));
 }
